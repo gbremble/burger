0676ca52a1300915650429121de8cc0375ff1ba4
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-07-31 00:32:56 -0400

    created db seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Baconator', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Royal Red Robin', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('McDouble', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Whopper', false);
