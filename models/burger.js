147d9c91b30e6de992a570a85efd6061b4b2f978
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-08-02 18:29:57 -0400

    added code for burger model

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,26 @@
 var orm = require('../config/orm.js');
 
 var burger = {
-    // add code specific to the burger app to interface with the ORM
-}
+    all: function (callback) {
+        orm.all('burgers', function (res) {
+            callback(res);
+        });
+    },
+    create: function (cols, vals, callback) {
+        orm.create('burgers', cols, vals, function (res) {
+            callback(res);
+        });
+    },
+    update: function (objColVals, condition, callback) {
+        orm.update('burgers', objColVals, condition, function (res) {
+            callback(res);
+        });
+    },
+    delete: function (condition, callback) {
+        orm.delete('burgers', condition, function (res) {
+            callback(res);
+        });
+    }
+};
 
 module.exports = burger;
