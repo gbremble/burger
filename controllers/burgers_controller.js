e219cc74fe6d11ed94161ab2b7b5a520c7c972b7
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-08-03 21:18:46 -0400

    set up controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,76 @@
+// ### IMPORT EXPRESS ###
+var express = require('express');
+var router = express.Router();
+
+// ### IMPORT MODEL ###
+var burger = require('../models/burger.js');
+
+router.get('/', function (req, res) {
+    burger.all(function (data) {
+        var burgerObject = {
+            burgers: data
+        };
+        console.log(burgerObject);
+
+        // display the returned database data
+        res.render('index', burgerObject);
+    });
+});
+
+router.post('api/burgers', function (req, res) {
+    burger.create([
+        'burger_name', 'devoured'
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
+        res.json({
+            id: result.insertId
+        });
+    });
+});
+
+router.put('api/burgers/:id', function (req, res) {
+    var condition = 'id = ' + req.params.id;
+
+    console.log('condition', condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // if no rows were changed, return '404 not found'
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete('/api/burgers/:id', function (req, res) {
+    var condition = 'id = ' + req.params.id;
+
+    burger.delete(condition, function (result) {
+        if (result.affectedRows == 0) {
+            // if no rows will be deleted, return '404 not found'
+            return res.status(404).end();
+        } else {
+            return res.status(200).end();
+        }
+    });
+});
+
+// ### EXPORT ### Export the routes for server.js
+module.exports = router;
+
+
+
+// // Create all our routes and set up logic within those routes where required.
+// router.get("/", function(req, res) {
+//   cat.all(function(data) {
+//     var hbsObject = {
+//       cats: data
+//     };
+//     console.log(hbsObject);
+//     res.render("index", hbsObject);
+//   });
+// });
