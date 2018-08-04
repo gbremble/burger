2132bc84d960cbbcbfccc1939f86bc40bfeb9e35
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-08-03 22:54:44 -0400

    updated to fix server.js and added public folder structure

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 // Set Handlebars.
 var exphbs = require('express-handlebars');
 
-app.engine('handlebars' exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access.
